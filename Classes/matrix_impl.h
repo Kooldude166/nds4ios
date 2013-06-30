ec95c30ec9decb5b6f4b0c6af7ddafb81ce62298
David Chavez <david@psychosity.net>
2013-06-29 23:31:55 -0500

    Added extra - icons for iOS 7, correced matrix files from original source.

--- Classes/matrix_impl.h
+++ Classes/matrix_impl.h
@@ -1,8 +1,7 @@
 /*
- VFP math library for the iPhone / iPod touch
+ NEON math library for the iPhone / iPod touch
  
- Copyright (c) 2007-2008 Wolfgang Engel and Matthias Grundmann
- http://code.google.com/p/vfpmathlibrary/
+ Copyright (c) 2009 Justin Saunders
  
  This software is provided 'as-is', without any express or implied warranty.
  In no event will the authors be held liable for any damages arising
@@ -22,50 +21,21 @@
  3. This notice may not be removed or altered from any source distribution.
  */
 
-#ifndef MATRIX_IMPL_H__
-#define MATRIX_IMPL_H__
+#ifndef __NEON_MATRIX_IMPL_H__
+#define __NEON_MATRIX_IMPL_H__
+
+#ifdef __arm__
+#import "arm/arch.h"
+#endif
 
 // Matrixes are assumed to be stored in column major format according to OpenGL
 // specification.
 
-// Multiplies two 4x4 matrices.
-void Matrix4Mul(const float* src_mat_1, const float* src_mat_2, float* dst_ptr);
+// Multiplies two 4x4 matrices (a,b) outputing a 4x4 matrix (output)
+void NEON_Matrix4Mul(const float* a, const float* b, float* output );
 
-// Multiplies a 4x4 matrix with a 4-dim. vector.
-void Matrix4Vector4Mul(const float* src_mat, const float* src_vec, float* dst_vec);
-
-// Multiplies a 4x4 matrix with a 3-dim. vector. Last coordinate is assumed to be 1.
-// Output is 4-dim.
-void Matrix4Vector3Mul(const float* src_mat, const float* src_vec, float* dst_vec);
-
-// Multiplies a 4x4 matrix with a 3-dim. vector. Last coordinate is assumed to be w.
-// Output is 4-dim.
-void Matrix4Vector3Mul(const float* src_mat, const float* src_vec, float w, float* dst_vec);
-
-// Multiplies a 4x4 matrix with a 3-dim. vector. Last coordinate is assumed to be 1.
-// Output is 4-dim.
-void Matrix4Vector3ArrayMul(int num,                          // Number of Vertices.
-                            const float* src_mat,             // Source matrix.
-                            int src_stride,                   // Source vector stride in bytes.
-                            const float* src_vec_array,       // Source vector array.
-                            int dst_stride,                   // Dest. vector stride in bytes.
-                            float* dest_vec_array);           // Dest. vector array.
+// Multiplies a 4x4 matrix (m) with a vector 4 (v), outputing a vector 4
+void NEON_Matrix4Vector4Mul(const float* m, const float* v, float* output);
 
 
-// Multiplies a 4x4 matrix with a 3-dim. vector. Last coordinate is assumed to be w.
-// Output is 4-dim.
-void Matrix4Vector3ArrayMul(int num,                          // Number of Vertices.
-                            const float* src_mat,             // Source matrix.
-                            float w,                          // Last coordinate of vectors.
-                            int src_stride,                   // Source vector stride in bytes.
-                            const float* src_vec_array,       // Source vector array.
-                            int dst_stride,                   // Dest. vector stride in bytes.
-                            float* dest_vec_array);           // Dest. vector array.
-
-// Inverts a 4x4 Matrix with 94 multiplications and one division.
-// This is not the fastest possible implementation (60 mult + 2 divisions) but it is
-// not dependent on the determinants of submatrices.
-// Furthermore on iPhone, division has IPC of 15 vs. 1 IPC for multiplication.
-void Matrix4Invert(const float* src_mat, float* dst_mat);
-
-#endif // MATRIX_IMPL_H__
+#endif // __NEON_MATRIX_IMPL_H__
