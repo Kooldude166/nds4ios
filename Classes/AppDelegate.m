e242a8c4e45003bb8dc4d23a884bbc1ace701338
David Chavez <david@psychosity.net>
2013-07-02 15:01:23 -0500

    Removed unnecessary code

--- Classes/AppDelegate.m
+++ Classes/AppDelegate.m
@@ -6,14 +6,7 @@
 //  Copyright (c) 2012 Homebrew. All rights reserved.
 //
 
-#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
-#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
-#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
-#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
-#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
-
 #import "AppDelegate.h"
-
 #import "RomsViewController.h"
 #import "SwitcherViewController.h"
 #import "EmuViewController.h"
